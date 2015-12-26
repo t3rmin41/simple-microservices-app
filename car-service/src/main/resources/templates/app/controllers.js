317a9af2b840fee0f03e015744cf02b708320d46
t3rmin41 <t33rmin41@gmail.com>
2015-12-26 16:23:37 +0200

    added angular-route module

--- car-service/src/main/resources/templates/app/controllers.js
+++ car-service/src/main/resources/templates/app/controllers.js
@@ -1,7 +1,15 @@
-angular.module('carsApp.controllers', []);
+var app = angular.module('carsApp.controllers', []);
 
-angular.module('carsApp.controllers').controller('CarController', function($scope, Entry){
-    var entries = Entry.query(function() {
-        console.log(entries);
-    });
-});
+app.controller('CarController', ['$scope', 'CarFactory', function($scope, CarFactory){
+    CarFactory.get({}, function(carFactory){
+        var cars = CarFactory.query(function(){
+            console.log(cars);
+        })
+    })
+}]);
+
+//angular.module('carsApp.controllers').controller('CarController', function($scope, Entry){
+//    var entries = Entry.query(function() {
+//        console.log(entries);
+//    });
+//});
--- car-service/src/main/resources/templates/app/services.js
+++ car-service/src/main/resources/templates/app/services.js
@@ -1,3 +1,14 @@
-angular.module('carsApp.services').factory('Entry', function($resource) {
-    return $resource('/cars/:id'); // Note the full endpoint address
+var services = angular.module('carsApp.services', ['ngResource']);
+
+services.factory('CarFactory', function($resource){
+    return $resource('/cars', {},{
+        query: {
+            method: 'GET',
+            params: {},
+        }
+    })
 });
+
+//angular.module('carsApp.services').factory('Entry', function($resource) {
+//    return $resource('/cars/:id'); // Note the full endpoint address
+//});
