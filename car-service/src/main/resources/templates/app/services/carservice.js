91ac0a6cfa3de526ab6bd6dcffa063102be3dcbe
t3rmin41 <t33rmin41@gmail.com>
2016-01-04 15:03:39 +0200

    Template for .then()

--- car-service/src/main/resources/templates/app/services/carservice.js
+++ car-service/src/main/resources/templates/app/services/carservice.js
@@ -3,9 +3,10 @@
 angular.module('carsApp.services', ['ngResource']).factory('CarService', function ($http, $q) {
     return {
         getCars : function(carId) {
+            /**/
             var promise;
             if (carId != undefined) {
-                promise = $http({ method : 'GET', url : '/cars/' + carId, isArray : true})
+                promise = $http({ method : 'GET', url : '/cars/' + carId})
                     .success(function (data, status, headers) {
                         console.log('Data in service carId == ' + carId);
                         console.log(data);
@@ -14,7 +15,7 @@ angular.module('carsApp.services', ['ngResource']).factory('CarService', functio
                         console.log('error');
                     });
             } else {
-                promise = $http({ method : 'GET', url : '/cars/', isArray : true })
+                promise = $http({ method : 'GET', url : '/cars/'})
                     .success(function (data, status, headers) {
                         console.log('Data in service');
                         console.log(data);
@@ -24,6 +25,34 @@ angular.module('carsApp.services', ['ngResource']).factory('CarService', functio
                     });
             }
             return promise;
+            /**/
+            /*
+            if (carId != undefined) {
+                return $http({ method : 'GET', url : '/cars/' + carId})
+                    .then(function (response) {
+                        console.log('Data in service carId == ' + carId);
+                        console.log(response.data);
+                    })
+                    .catch(function (response) {
+                        console.log('error');
+                    })
+                    .finally(function (response) {
+                        console.log('Finished getting from service');
+                    });
+            } else {
+                return $http({ method : 'GET', url : '/cars/'})
+                .then(function (response) {
+                    console.log('Data in service');
+                    console.log(response.data);
+                })
+                .catch(function (response) {
+                    console.log('error');
+                })
+                .finally(function (response) {
+                    console.log('Finished getting from service');
+                });
+            }
+            /**/
         }
     }
 });
